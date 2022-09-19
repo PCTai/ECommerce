63e64a028ec32c115968860878f0f1cbc50d64be
PCTai <pntai121101@gmail.com>
2022-09-19 15:52:55 +0700

    light-dart-mode

--- script/app.js
+++ script/app.js
@@ -55,7 +55,7 @@ window.addEventListener('load', function(){
         }
     ]
     const overviewMain= this.document.querySelector('.overview-container');
-    const overviewPaginations= this.document.querySelectorAll('.slide-item');
+    const overviewPaginations= this.document.querySelectorAll('.overview-slide .slide-item');
     const pageItems =1;
     let start= 0;
     let end=pageItems; 
@@ -63,7 +63,7 @@ window.addEventListener('load', function(){
 
     function renderOverViewMain(){
         let html='';
-        listOverView.filter((item, index) =>{
+        listOverView.forEach((item, index) =>{
             if(index===start && index<end){
                 html+= 
             "<div class= " +"'overview-content'" +">"+
@@ -87,16 +87,27 @@ window.addEventListener('load', function(){
     for(let i= 0; i< overviewPaginations.length; i++){
         overviewPaginations[i].addEventListener('click',function(){
             document.querySelector('.slide-item.active').classList.remove('active');
-            currentIndex++;
+            currentIndex=i +pageItems;
+            console.log(currentIndex);
             start=i; 
             end= currentIndex;
             overviewPaginations[i].classList.add('active');
             renderOverViewMain();
         })
     }
-
-
-
+    // autoNext
+    // setInterval(()=>{
+        
+    //     console.log(currentIndex);
+    //     if(currentIndex>=0 && currentIndex < overviewPaginations.length){
+    //         overviewPaginations[currentIndex].click();
+    //     }
+    //     else{
+    //         currentIndex=0;
+    //     }
+    //     currentIndex++;
+    // },2000)
+   
     // slider
     if(index===0){
         btnPrev.style= 'opacity: 0.6';
@@ -147,5 +158,136 @@ window.addEventListener('load', function(){
             listProduct.style= `transform: translateX(${positionX}px)`
         }
     }
+
+    // project
+
+    const listProjects=[
+        {
+            id: 1,
+            title: "Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/product1.png"
+        },
+        {
+            id: 2,
+            title: "2 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/pr2.png"
+        },
+        {
+            id: 3,
+            title: "3 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/pr3.png"
+        },
+        {
+            id: 4,
+            title: "4 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/pr4.png"
+        },
+        {
+            id: 5,
+            title: "5 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/product1.png"
+        },
+        {
+            id: 6,
+            title: "6 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/pr2.png"
+        },
+        {
+            id: 7,
+            title: "7 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/pr3.png"
+        },
+        {
+            id: 8,
+            title: "8 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/pr4.png"
+        },
+        {
+            id: 9,
+            title: "9 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/product1.png"
+        },
+        {
+            id: 10,
+            title: "10 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/pr2.png"
+        },
+        {
+            id: 11,
+            title: "11 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/pr3.png"
+        },
+        {
+            id: 12,
+            title: "12 Bedroom Tiles",
+            decription:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            image: "./images/product1.png"
+        }
+    ]
+
+    const projectMain= this.document.querySelector('.project-list');
+    const projectPaginations= this.document.querySelectorAll('.project-slide .slide-item');
+    const ProjectPageItem =8;
+    let projectStart= 0;
+    let projectEnd=ProjectPageItem; 
+    let projectCurrentIndex= 1;
+
+    function renderProjectMain(){
+        let html='';
+        listProjects.forEach((item, index) =>{
+            if(index>=projectStart && index <projectEnd){
+
+                // console.log(projectStart, projectEnd, index);
+                html +="<div class="+"'project-item '" +">" +
+                "<img src="+`'${item.image}'` +">"+
+                "<div class="+"'project-content'"+">"+
+                    "<h5>"+`${item.title}` +"</h5>"+
+                "<span>"+`${item.decription}` +"</span>"+
+                "</div></div>"
+            }
+        })
+        document.querySelector('.project-list').innerHTML = html;
+    }
+    renderProjectMain();
+    for(let j= 0; j< projectPaginations.length; j++){
+        
+        projectPaginations[j].addEventListener('click',function(){
+            document.querySelector('.project-slide .slide-item.active').classList.remove('active');
+            projectCurrentIndex = j +1;
+            projectStart=j* ProjectPageItem; 
+            projectEnd= projectCurrentIndex * ProjectPageItem;
+            
+            projectPaginations[j].classList.add('active');
+            renderProjectMain();
+        })
+    }
+
+    // light mode
+
+    const lightMode= document.querySelector('.lightMode .lightMode-circle');
+    const wrapperLightMode= document.querySelector('.lightMode');
+    const activeLightMode= document.querySelector('.root');
+    lightMode.addEventListener('click' , function(){
+        activeLightMode.classList.toggle('active');
+        if(document.querySelector('.root.active')){
+            lightMode.style="right: 0";
+        }
+        else{
+            lightMode.style="left: 0";
+        }
+        
+        
+    })
 
 })
